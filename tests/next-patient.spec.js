f7ca8c09e05c1fc7a24f2d808a9135becd2540d1
Spectoor <saidomar@et.esiea.fr>
2023-05-14 23:38:48 +0200

    Test strategy

---
+++ tests/next-patient.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const AxeBuilder = require('@axe-core/playwright').default;
+
+test('Next Patient', async ({ page }) => {
+  await page.goto('');
+  await expect(page).toHaveScreenshot();
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag2aaa', 'best-practice'])
+    .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
