9913de2fb71a2ecfcb703f53d91fc33c79c0bbb3
Spectoor <saidomar@et.esiea.fr>
2023-05-16 17:06:49 +0200

    Update Home snapshot

--- tests/next-patient.spec.js
+++ tests/next-patient.spec.js
@@ -6,7 +6,7 @@ test('Next Patient', async ({ page }) => {
   await page.goto('');
   await expect(page).toHaveScreenshot();
   const accessibilityScanResults = await new AxeBuilder({ page })
-    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag2aaa', 'best-practice'])
+    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
     .analyze();
 
   expect(accessibilityScanResults.violations).toEqual([]);
